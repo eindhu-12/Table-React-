ba57e5b62a66516de16de0bdfb734d80f6417970
unknown <rapoluindhu2929@gmail.com>
2025-02-09 22:50:33 +0530

    Serivce Layer Folder

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Staff from './Components/Staff/Staff'
+import Student from './Components/Students/Student'
+import Transport from './Components/Transport/Transport'
+
+function App() {
+
+  return (
+    <>
+    <Staff/>
+    <Student/>
+    <Transport/>
+
+
+    </>
+  )
+}
+
+export default App
